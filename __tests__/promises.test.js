a0e723941e5aee74b5ee6182f0cda5bcf50fcdcb
AngelDOrtiz <aortizghs@gmail.com>
2021-06-24 09:59:22 -0700

    test passing

--- __tests__/promises.test.js
+++ __tests__/promises.test.js
@@ -7,20 +7,6 @@ describe('promise route', () => {
     const res = await request(promises)
       .get('/index.html');
 
-    expect(res.text).toEqual(`<!DOCTYPE html>
-      <html lang="en">
-
-      <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-      </head>
-
-      <body>
-          <h2>You did it!!</h2>
-      </body>
-      
-      </html>`);
+    expect(res.text).toEqual('<h2>hello</h2>');
   });
 });
